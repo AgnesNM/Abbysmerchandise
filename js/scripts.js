457956ec47b8515542dc7f3a8a08c8825b08e643
Agnes NM <agnesndutamwaniki@gmail.com>
2019-03-15 10:07:58 +0300

    add sign up form

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//Business Logic
+function user (name, email, password {
+  this.name = name;
+  this.
+})
+//UI Logic
+$(document).ready(function(){
+  $("#formToggle").click(function(){
+    $(".userInput").show();
+    $("#signup").hide();
+  })
+  $(".userInput").submit(function(event){
+    var userName = $("newName").val();
+    var userEmail = $("newEmail").val();
+    var userPass = $("newPass").val();
+    var newUser =
+  });
+  eventPreventDefault();
+});
