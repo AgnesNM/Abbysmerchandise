d3538a3e0e8e76a1cf40d9094cc843c91906e13f
Agnes NM <agnesndutamwaniki@gmail.com>
2019-04-06 09:53:12 +0300

    add functionality to collect price and decoration costs, and them up, works for only the first product this far

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+//Business Logic
+function totalCost (decoCost,cost){
+	return decoCost + cost;
+ }
 //UI Logic
 $(document).ready(function() {
 	$("#userSignUp").click(function() {
@@ -16,38 +20,57 @@ $(document).ready(function() {
 		$("#loginForm").show();
 		$("#login").hide();
 	});
-	$("#userData").click(function(e) {
+	$("#userData").click(function(event) {
 		event.PreventDefault;
-		var custName = $("#newName").val();
-		var custEmail = $("#newEmail").val();
-		var custPass = $("#newPass").val();
-		var custData = [];
-		custData.push(custName, custEmail, custPass);
+
 		var currentName = $("#userName").val();
 		var currentEmail = $("#userEmail").val();
 		var currentPass = $("#userPass").val();
 			if ((currentName === custData[0]) && (currentEmail === custData[1]) && (currentPass === custData[2])) {
-				alert('You are looged in!');
+				alert('You are logged in!');
 			}
 		});
 
 	$("#price").click(function(){
 		var cost = $("#price").val();
 		var choice = prompt("This card holder costs 700. Would you like to choose a decoration type?");
-			if (choice != null && choice === "yes") {
+			if (!null &&choice === "yes") {
 				$("#display").text("Great, please,go ahead!");
 				$("#preference").show();
 			} else{
-				$(".options").hide();
+
 				$("#display").text("Your total cost is " + cost);
 			}
 	});
 
-	$("#preference").click(function(){
-		var cost = $("#price").val();
-		var decoCost = $("#decoChoice").find(":selected").attr('value');
-		var totalCost = parseInt(cost) + parseInt(decoCost);
-		alert("Your total cost is KES: " + totalCost);
-		alert("Thank you for purchasing from us!");
+	$("#decoPrice").click(function(){
+		var decoCost = $("#decoPrice").val();
+		$("#displayArea").text("This decoration costs " + decoCost);
+		$("#decoOptions").show();
 	});
+	$("#itemPrice").click(function(){
+		var cost = $("#itemPrice").val();
+		$("#displayArea").text("This item costs " + cost);
+	});
+	$("#decoPreference").click(function(){
+		var decoCost = $("#decoPrice").val();
+		var cost = $("#itemPrice").val();
+		var result = parseInt(decoCost) + parseInt(cost);
+		$("#displayArea").text("Your total cost is " + result);
+	});
+
+
+		// $("#decoPreference").click(function(){
+		// 	var cost = $("#price").val();
+		// 	var decoCost = $("#decoPrice").val();
+		// 		alert(cost);
+		// 		alert(decoCost);
+		// });
+
+
+		// var decoCost = $("#decoChoice").val();
+		// var totalCost = parseInt(cost) + parseInt(decoCost);
+		// alert("Your total cost is KES: " + totalCost);
+		// alert("Thank you for purchasing from us!");
+	 // });
 });
